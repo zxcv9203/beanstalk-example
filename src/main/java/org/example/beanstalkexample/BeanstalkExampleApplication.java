8eb685b531d265970adc708ed41b99c0816f50b4
yongckim <zxcv9203@naver.com>
2024-02-07 22:18:37 +0900

    :tada: 프로젝트 초기화

---
+++ src/main/java/org/example/beanstalkexample/BeanstalkExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.beanstalkexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanstalkExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanstalkExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/beanstalkexample/BeanstalkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.beanstalkexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanstalkExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
